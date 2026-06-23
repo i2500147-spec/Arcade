44635b804fe9f14d68f5199b368d76e40f535095
i2500147-spec <i2500147@gmail.com>
2026-06-23 20:40:12 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ def log_error(exc_type, exc_value, exc_tb):
 sys.excepthook = log_error
 print("Starting bot...")
 
-BOT_TOKEN = "8941049801:AAFQHjVBXnx_58ndwkskRTwEam0g5ZaZcb0"
+BOT_TOKEN = "8941049801:AAG2nZRdD-56HnQflA48bvZKrZ0_8lhdEOE"
 BOT_USERNAME = "arcadecasinobot"
 OWNER_ID = 8131755675
 TON_WALLET = "UQAISFpye-QozqPlK1iX_qHPmYzEphSNalQsFojALxuLXpx6"
